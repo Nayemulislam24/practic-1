c6690708167fcae1bc3cdfc859ab4fd7efff8010
MD. Zannatun Nayem <124041305+Nayemulislam24@users.noreply.github.com>
2023-02-16 15:48:01 +0600

    add php file

---
+++ news-template-1/admin/config.php
@@ -0,0 +1,4 @@
+<?php
+$hostname = "http://localhost/news-template";
+$conn = mysqli_connect("localhost","root","","news-site") or die("connection faild".mysqli_connect_errors());
+?>
---
+++ news-template-1/admin/delete-user.php
@@ -0,0 +1,15 @@
+<?php
+include "config.php";
+if($_SESSION["user_role"] == "0"){
+    header("Location: {$hostname}/admin/post.php");
+ }
+$userid = $_GET['id'];
+$sql = "DELETE FROM user WHERE user_id = {$userid}";
+if(mysqli_query($conn,$sql)){
+    header("Location: {$hostname}/admin/users.php");
+}else{
+    echo"<p style='color:red;margin: 10px 0;'>can\.t Delete the user Record.</p>";
+
+}
+mysquli_close($conn);
+?>
